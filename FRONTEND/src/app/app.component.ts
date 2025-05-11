a74ad37e6b1b9f2a2529660fae8d59d2ea49286a
Adam Revesz <revike6@gmail.com>
2025-05-11 19:40:03 +0200

    Created the register design and added favicon

--- FRONTEND/src/app/app.component.ts
+++ FRONTEND/src/app/app.component.ts
@@ -3,12 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, RouterModule, LoginComponent],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    FooterComponent, 
+    RouterModule, 
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- FRONTEND/src/app/app.routes.ts
+++ FRONTEND/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './components/login/login.component';
-
+import { RegisterComponent } from './components/register/register.component';
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, title: 'Home' },
-  {path: 'login', component: LoginComponent, title: 'Login'},
+  { path: 'login', component: LoginComponent, title: 'Login'},
+  { path: 'register', component: RegisterComponent, title: 'Register'},
 ];
